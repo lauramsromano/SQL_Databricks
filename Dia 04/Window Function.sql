dd33bec44df18f75d3218bdb2df4a3d33eaa097c
Laura Romano <Laura Romano>
2022-12-17 01:03:55 +0000

    Window Function

---
+++ Dia 04/Window Function.sql
@@ -0,0 +1,46 @@
+-- Databricks notebook source
+-- DBTITLE 1,Window Function:
+WITH tb_vendas_vendedores AS (
+
+  SELECT 
+        idVendedor,
+        COUNT(*) As qtVendas
+  
+  FROM silver_olist.item_pedido
+  
+  GROUP BY idVendedor
+  ORDER BY qtVendas DESC
+
+),
+
+tb_row_number AS (
+
+  SELECT T1.*,
+         T2.descUf,
+         ROW_NUMBER() OVER (PARTITION BY T2.descUf ORDER BY qtVendas DESC) AS RN
+
+  FROM tb_vendas_vendedores AS T1
+
+  LEFT JOIN silver_olist.vendedor AS T2
+  ON T1.idVendedor = T2.idVendedor
+
+  QUALIFY RN <= 10
+
+  ORDER BY descUF, qtVendas DESC
+)
+  
+SELECT * FROM tb_row_number
+
+-- COMMAND ----------
+
+-- Recaptulando:
+
+SELECT *
+
+FROM A
+
+WHERE -- FILTRA NA FONTE
+
+QUALIFY -- FILTRA WINDOW FUNCTION
+
+HAVING -- FILTRA GROUP BY
